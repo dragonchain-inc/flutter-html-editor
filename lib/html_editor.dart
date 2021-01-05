9f2d58e63602dafb8bfbb12d019d5a2b7f606fe1
Todd Wilson <taw1313@gmail.com>
2021-01-05 17:58:11 -0500

    add debug

--- lib/html_editor.dart
+++ lib/html_editor.dart
@@ -213,8 +213,8 @@ class HtmlEditorState extends State<HtmlEditor> {
     print('!!!!!!!!!!!!!! DEGUG getText()');
     await _controller.evaluateJavascript(
         "console.log(document.getElementsByClassName('note-editable')[0].innerHTML);");
-    await _controller.evaluateJavascript(
-        "GetTextSummernote.postMessage(document.getElementsByClassName('note-editable')[0].innerHTML);");
+    // await _controller.evaluateJavascript(
+    // "GetTextSummernote.postMessage(document.getElementsByClassName('note-editable')[0].innerHTML);");
     print('!!!!!!!!!!!!!!');
     return text;
   }
