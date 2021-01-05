d2d987e796189abadfd52497433924e9ae26d6ba
Todd Wilson <taw1313@gmail.com>
2021-01-05 16:51:44 -0500

    change how to pull html data

--- lib/html_editor.dart
+++ lib/html_editor.dart
@@ -210,8 +210,10 @@ class HtmlEditorState extends State<HtmlEditor> {
   }
 
   Future<String> getText() async {
+    // await _controller.evaluateJavascript(
+    //     "GetTextSummernote.postMessage(document.getElementsByClassName('note-editable')[0].innerHTML);");
     await _controller.evaluateJavascript(
-        "GetTextSummernote.postMessage(document.getElementsByClassName('note-editable')[0].innerHTML);");
+        "GetTextSummernote.postMessage('\$('.note-editable').html()');");
     return text;
   }
 
